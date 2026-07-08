b9ea905d3c43100099837c3cae9cef12d9e93832
acegreen1 <135817768+acegreen1@users.noreply.github.com>
2026-07-08 17:35:20 +0530

    libBchat Swift Package Manager 1.1.0

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BchatUtil",
-            url: "https://github.com/Beldex-Coin/libbchat-util-spm/releases/download/1.0.0/libbchat-util.xcframework.zip",
-            checksum: "PLACEHOLDER_UNTIL_FIRST_BUILD"
+            url: "https://github.com/abdevil05/libbchat-util-spm/releases/download/1.1.0/libbchat-util.xcframework.zip",
+            checksum: "0098ebd0dcd4579e62a91b45f82409751a485ae710bbc94621a2ff4d7c0bd107"
         )
     ]
 )
